b14aeffe380ff987b30b3402cba11b844248e545
levAriUlnerWork <levariulner@gmail.com>
2020-12-29 11:45:45 +0200

    commit

--- Client.py
+++ Client.py
@@ -1,1 +1,22 @@
 import socket
+
+HEADER_TCP = 20
+HEADER_UDP = 8
+PORT = 5010 #?
+OUR_PORT = 2014
+FORMAT = 'utf-8'
+SERVER = socket.gethostbyname(socket.gethostname) # Should be 172.l.0.14
+ADDR = (SERVER,PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_len = len(message)
+    send_len = str(msg_len).encode(FORMAT)
+    padded_send_len = b' ' * (HEADER_TCP-len(send_len))
+    client.send(send_len)
+    client.send(message)
+
+send("Hello!")
--- Server.py
+++ Server.py
@@ -1,2 +1,28 @@
 import socket
 import threading
+
+HEADER_TCP = 20
+HEADER_UDP = 8
+SERVER = socket.gethostbyname(socket.gethostname) # Should be 172.l.0.14
+PORT = 5010 #?
+OUR_PORT = 2014
+ADDR = (SERVER,PORT) 
+FORMAT = 'utf-8'
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_clients_before_game(conn, addr):
+    print(f"Received offer from")
+
+
+def start():
+    server.listen()
+    print (f"Server started, listening on IP address {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_clients_before_game, args=(conn,addr))
+        thread.start()
+
+
+start()
