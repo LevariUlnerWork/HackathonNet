5e44ab538e714fc6a7c8c19e35b132d6b5c37ab1
levAriUlnerWork <levariulner@gmail.com>
2020-12-30 11:36:02 +0200

    commit

--- Client.py
+++ Client.py
@@ -2,16 +2,38 @@ import socket
 
 HEADER_TCP = 20
 HEADER_UDP = 8
-PORT = 5010 #?
-OUR_PORT = 2014
+SERVER_PORT = 0
+PORT = 0
+SERVER_ADDR = 0
 FORMAT = 'utf-8'
 SERVER = socket.gethostbyname(socket.gethostname) # Should be 172.l.0.14
 ADDR = (SERVER,PORT)
 diconnect_msg = "disconnect"
 
+
+clientUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) # UDP
+clientUDP.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+
+# Enable broadcasting mode
+clientUDP.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
+clientUDP.bind(("", 13117))
+while True:
+    print("Client started, listening for offer requests... ")
+    data, addr = clientUDP.recvfrom(1024)
+    message = str(data[7:]).decode(FORMAT)
+    address = message.split('address ')[1]
+    if((data[:5] == bytes.fromhex('feedbeef') + bytes.fromhex('02')) and len(SERVER_ADDR)<12):
+        dest_port = data[5].hex()
+        dest_port_number = int(dest_port,16)
+        SERVER_ADDR = (address,dest_port_number)
+        break
+print(f"Received offer from {SERVER_ADDR} , attempting to connect...")
 
 def send(msg):
     message = msg.encode(FORMAT)
@@ -21,7 +43,4 @@ def send(msg):
     client.send(send_len)
     client.send(message)
 
-while True:
-    serverMsg = client.recv(100)
-    print serverMsg.decode(FORMAT)
 send("Hello!")
--- Server.py
+++ Server.py
@@ -1,9 +1,9 @@
 import socket
 import threading
+import time
 
 
-HEADER_UDP_MAGIC_COOKIE = int('feedbeef',16)
-MESSAGE_OFFER_TYPE = 2
+
 SERVER = socket.gethostbyname(socket.gethostname) # Should be 172.l.0.14
 BROADCAST_PORT = 13117
 SERVER_PORT = 2014
@@ -11,31 +11,57 @@ ADDR = (SERVER,SERVER_PORT)
 FORMAT = 'utf-8'
 diconnect_msg = "disconnect"
 
-UDPserver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-UDPserver.bind(ADDR)
 
-def handle_clients_before_game(conn, addr):
-    print(f"new connection {addr} Received offer from")
-    connected = True
-    while connected:
-        msg_length = conn.recv(HEADER_UDP_MAGIC_COOKIE).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == diconnect_msg:
-                connected = False
-            print(f"[{addr}] {msg}")
-            conn.send("msg recieved".encode(FORMAT))
-    conn.close()
+#FOR THE GAME:
+ALL_TIME_TABLE = {}
+GROUP1 = {} # {addr:number of packets}
+GROUP2 = {} # {addr:number of packets}
+
+
+
+broadcastServer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+# Enable broadcasting mode
+broadcastServer.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+broadcastServer.settimeout(0.2)
+
+TCPserver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+TCPserver.bind(ADDR)
+
+def broadcast_message_before_game():
+    header = bytes.fromhex('feedbeef') + bytes.fromhex('02') + bytes.fromhex('07de')
+    messageBytes = ("Server started, listening on IP address 172.1.0.14").encode(FORMAT)
+    broadcast_message = header + messageBytes
+    while True:
+        broadcastServer.sendto(broadcast_message, ('<broadcast>', 13117))
+        time.sleep(1)
+    
+
+# def handle_clients_before_game(conn, addr):
+#     print(f"new connection {addr} Received offer from")
+#     connected = True
+#     while connected:
+#         msg_length = conn.recv(HEADER_UDP_MAGIC_COOKIE).decode(FORMAT)
+#         if msg_length:
+#             msg_length = int(msg_length)
+#             msg = conn.recv(msg_length).decode(FORMAT)
+#             if msg == diconnect_msg:
+#                 connected = False
+#             print(f"[{addr}] {msg}")
+#             conn.send("msg recieved".encode(FORMAT))
+#     conn.close()
 
 def start():
-    UDPserver.listen()
+    broadcastServer.listen()
     print (f"Server started, listening on IP address {SERVER}")
     while True:
-        conn, addr = UDPserver.accept()
-        thread = threading.Thread(target=handle_clients_before_game, args=(conn,addr))
+        conn, addr = broadcastServer.accept()
+        thread = threading.Thread(target=broadcast_message_before_game, args=(conn,addr))
         thread.start()
-        print(f"Active connction: {threading.activeCount()-1}")
+        thread.join(10)
+    conn.close()
 
-print("Server started, listening on IP address * ")
-start()
+forever = True
+while forever:
+    isThereAreClients = False
+    while not isThereAreClients:
+        start()
